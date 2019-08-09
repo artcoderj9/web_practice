30eddd5bec471764aa8766bcbc0078dea5ab287c
jhjeong <jhjeong@hssoft3d.com>
2019-08-09 17:15:41 +0900

    jupyter notebook bat

---
+++ python/study/start_jupyter_notebook_here.bat
@@ -0,0 +1,8 @@
+@echo off
+rem ================================================
+rem ==  [how to use]
+rem ==  1. run anaconda (powershell) prompt 
+rem ==  2. run start_jupyter_notebook_here.bat  
+rem ================================================
+ 
+jupyter notebook
